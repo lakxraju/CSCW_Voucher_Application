a636b5abbf3f60ef3070df4aabd6605663f06e9b
SharathChandra <sharathchandra91@gmail.com>
2016-07-14 01:41:24 +0200

    Block details added in History. Minor UI changes.

--- Client/js/main.js
+++ Client/js/main.js
@@ -3,7 +3,7 @@
  created on  01/07/2016
  */
 
-var app = angular.module('app', ["ui.bootstrap", "ngAnimate", "ngRoute", "ngCookies", "blockUI", "smart-table", "trNgGrid"]);
+var app = angular.module('app', ["ui.bootstrap", "ngAnimate", "ngRoute", "ngCookies", "blockUI", "smart-table", "trNgGrid", "ngPrettyJson"]);
 
 /**
  * Configure the Routes
@@ -18,7 +18,6 @@ app.config(['$routeProvider', function ($routeProvider) {
         .when("/main", {templateUrl: "partials/main.html", controller: "voucherCtrl"})
 
 }]);
-
 
 app.controller('LoginCtrl', function ($scope, $http, $location, $cookies) {
 
@@ -108,6 +107,7 @@ app.controller('voucherCtrl', function ($scope, $filter, $http, $uibModal, $cook
 
     $scope.nameOfVoucher = "";
     $scope.valueOfVoucher = "";
+    $scope.blockDetails = [];
 
     //$http.get('data/sample.json')
     //    .success(function (data) {
@@ -167,7 +167,6 @@ app.controller('voucherCtrl', function ($scope, $filter, $http, $uibModal, $cook
         // called asynchronously if an error occurs
         // or server returns response with an error status.
     });
-
 
     $http({
         method: 'GET',
@@ -230,7 +229,7 @@ app.controller('voucherCtrl', function ($scope, $filter, $http, $uibModal, $cook
                 blockUI.start("Transferring voucher...");
 
                 $timeout(function () {
-                    blockUI.message('Adding transaction to Bigchain ...');
+                    blockUI.message('Adding transaction to bigchain...');
                 }, 2000);
 
 
@@ -335,51 +334,32 @@ app.controller('voucherCtrl', function ($scope, $filter, $http, $uibModal, $cook
 
     };
 
-
-});
-
-app.controller('ModalInstanceCtrl2', function ($scope, $http, $uibModalInstance, items, $cookies, $window) {
-    $scope.items = items.data;
-
-    console.log(items);
-
-    $scope.ok = function () {
-
-        var trans_from = $cookies.get("user");
-        var vname = $scope.nameOfVoucher;
-        var vvalue = $scope.valueOfVoucher;
-
-        console.log(trans_from);
-        console.log(vname);
-        console.log(vvalue);
+    $scope.getBlockDetails = function (blkNumber) {
 
 
-        $http({
-            method: 'POST',
-            url: IPAddress + '/voucherApp/createVoucher',
-            headers: {'Content-Type': 'application/json'},
-            data: {
-                username: trans_from,
-                voucher_name: vname,
-                value: vvalue
+        console.log(blkNumber)
+        console.log("BLK NUMBER---")
+
+        $scope.blockNumber = blkNumber;
+
+        var modalInstance = $uibModal.open({
+            animation: $scope.animationsEnabled,
+            templateUrl: 'myModalContent3.html',
+            controller: 'ModalInstanceCtrl3',
+            windowClass: 'app-modal-window',
+            size: 'lg',
+            resolve: {
+                items: function () {
+                    return {
+                        'blkNum': $scope.blockNumber
+                    }
+                }
             }
-        }).success(function (data) {
-
-            console.log(data);
-
-            $uibModalInstance.close();
-            if (data.status == "error")
-                alert(data.errorMessage);
-            else
-                $window.location.reload();
 
         });
 
     };
 
-    $scope.cancel = function () {
-        $uibModalInstance.dismiss('cancel');
-    };
 });
 
 
@@ -398,7 +378,7 @@ app.controller('ModalInstanceCtrl', function ($scope, $http, $uibModalInstance, 
         blockUI.start("Transferring voucher...");
 
         $timeout(function () {
-            blockUI.message('Adding transaction to Bigchain ...');
+            blockUI.message('Adding transaction to bigchain...');
         }, 2000);
 
 
@@ -451,3 +431,77 @@ app.controller('ModalInstanceCtrl', function ($scope, $http, $uibModalInstance, 
         $uibModalInstance.dismiss('cancel');
     };
 });
+
+
+app.controller('ModalInstanceCtrl2', function ($scope, $http, $uibModalInstance, items, $cookies, $window) {
+    $scope.items = items.data;
+
+    console.log(items);
+
+    $scope.ok = function () {
+
+        var trans_from = $cookies.get("user");
+        var vname = $scope.nameOfVoucher;
+        var vvalue = $scope.valueOfVoucher;
+
+        console.log(trans_from);
+        console.log(vname);
+        console.log(vvalue);
+
+
+        $http({
+            method: 'POST',
+            url: IPAddress + '/voucherApp/createVoucher',
+            headers: {'Content-Type': 'application/json'},
+            data: {
+                username: trans_from,
+                voucher_name: vname,
+                value: vvalue
+            }
+        }).success(function (data) {
+
+            console.log(data);
+
+            if (data.status == "error")
+                alert(data.errorMessage);
+            else
+                $uibModalInstance.close();
+            $window.location.reload();
+
+        });
+
+    };
+
+    $scope.cancel = function () {
+        $uibModalInstance.dismiss('cancel');
+    };
+});
+
+
+app.controller('ModalInstanceCtrl3', function ($scope, $http, $uibModalInstance, items) {
+    $scope.blockNumber = items.blkNum;
+
+
+    $http({
+        method: 'GET',
+        url: IPAddress + '/voucherApp/getBlockContents?blockNumber=' + $scope.blockNumber,
+        headers: {'Content-Type': 'application/json'}
+    }).then(function successCallback(response) {
+        $scope.blockDetails = response.data
+
+        console.log($scope.items)
+        console.log("--------")
+
+
+    }, function errorCallback(response) {
+        // called asynchronously if an error occurs
+        // or server returns response with an error status.
+    });
+
+});
+
+
+
+
+
+
